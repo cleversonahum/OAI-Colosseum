72ccfb3128c4ae24ea09968d2b15de974187af17
Hai Cheng <cheng.hai@northeastern.edu>
2023-08-27 20:45:15 +0000

    update push .sh

--- utils/pull_oai_slicing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /root/Openairinterface5g/ &&  git pull 'https://ChengDaHaI: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@github.com/wineslab/Openairinterface.git' NR_Slicing
---
+++ utils/push_colo_OAI.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /root/OAI-Colosseum &&  git push 'https://ChengDaHaI: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@github.com/ChengDaHaI/OAI-Colosseum.git' slicing
