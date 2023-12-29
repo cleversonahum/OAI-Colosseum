8c2e6b06d379aa702adeaa5ed09ae70ab6c876ba
Cleverson Nahum <cleversonahum@gmail.com>
2023-12-29 23:28:25 +0000

    Creating a service to start all the services

--- utils/start_services.sh
+++ utils/start_services.sh
@@ -3,18 +3,17 @@
 ip_address=$(ip a show col0 | grep -Po "\b(?:\d{1,3}\.){3}\d{1,3}\b")
 third_octet=$(echo "$ip_address" | cut -d '.' -f 3)
 fourth_octet=$(echo "$ip_address" | cut -d '.' -f 4)
-abs_path=$(pwd)
 
 if [ "$third_octet" = "$fourth_octet" ]; then
 	echo "Enabling BS+Core services"
-	systemctl enable --now $abs_path/services/internet.service
-	systemctl enable --now $abs_path/services/core_network.service
-	systemctl enable --now $abs_path/services/gnb.service
+	systemctl enable --now /root/OAI-Colosseum/services/internet.service
+	systemctl enable --now /root/OAI-Colosseum/services/core_network.service
+	systemctl enable --now /root/OAI-Colosseum/services/gnb.service
 else
 	echo "Enabling UE services"
 	first_three_octets=$(echo $ip_address | cut -d '.' -f 1-3)
 	gnb_ip="$first_three_octets.$third_octet"
 	echo "GNB_IP=$gnb_ip" > /root/OAI-Colosseum/services/gnb.conf
-	systemctl enable --now $abs_path/services/internet.service
-	systemctl enable --now $abs_path/services/ue.service
+	systemctl enable --now /root/OAI-Colosseum/services/internet.service
+	systemctl enable --now /root/OAI-Colosseum/services/ue.service
 fi
