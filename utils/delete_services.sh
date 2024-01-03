55f1632de340a5407ff444944fa88de25f9bfe41
Cleverson Nahum <cleversonahum@gmail.com>
2024-01-03 17:03:18 +0000

    Fixing UE and gNB restarting and testing it during 3 UE's restarts

--- utils/delete_services.sh
+++ utils/delete_services.sh
@@ -3,15 +3,13 @@
 ip_address=$(ip a show col0 | grep -Po "\b(?:\d{1,3}\.){3}\d{1,3}\b")
 third_octet=$(echo "$ip_address" | cut -d '.' -f 3)
 fourth_octet=$(echo "$ip_address" | cut -d '.' -f 4)
-abs_path=$(pwd)
 
+systemctl disable --now start_network.service
 if [ "$third_octet" = "$fourth_octet" ]; then
 	echo "Disabling BS+Core services"
-	systemctl disable --now $abs_path/services/internet.service
-	systemctl disable --now $abs_path/services/core_network.service
-	systemctl disable --now $abs_path/services/gnb.service
+	systemctl disable --now core_network.service
+	systemctl disable --now gnb.service
 else
 	echo "Disabling UE services"
-	systemctl disable --now $abs_path/services/internet.service
-	systemctl disable --now $abs_path/services/ue.service
+	systemctl disable --now ue.service
 fi
