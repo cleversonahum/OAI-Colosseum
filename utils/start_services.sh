4526d623fbc5b3b4c08c17e3400fb46fba36fa58
Cleverson Nahum <cleversonahum@gmail.com>
2024-01-03 21:11:57 +0000

    Removing internet service since the dns is being update frequently and there is no need for permanent internet connection

--- utils/start_services.sh
+++ utils/start_services.sh
@@ -6,7 +6,6 @@ fourth_octet=$(echo "$ip_address" | cut -d '.' -f 4)
 
 if [ "$third_octet" = "$fourth_octet" ]; then
 	echo "Enabling BS+Core services"
-	systemctl enable --now /root/OAI-Colosseum/services/internet.service
 	systemctl enable --now /root/OAI-Colosseum/services/core_network.service
 	systemctl enable --now /root/OAI-Colosseum/services/gnb.service
 	systemctl enable --now /root/OAI-Colosseum/services/iperf_server.service
@@ -17,7 +16,6 @@ else
 	first_three_octets=$(echo $ip_address | cut -d '.' -f 1-3)
 	gnb_ip="$first_three_octets.$third_octet"
 	echo "GNB_IP=$gnb_ip" > /root/OAI-Colosseum/services/gnb.conf
-	systemctl enable --now /root/OAI-Colosseum/services/internet.service
 	systemctl enable --now /root/OAI-Colosseum/services/ue.service
 	systemctl enable --now /root/OAI-Colosseum/services/iperf_client.service
 fi
