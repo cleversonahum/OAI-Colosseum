e5d886289c3003744101f8f79a11509e56018fb0
Cleverson Nahum <cleversonahum@gmail.com>
2024-01-10 18:24:24 +0000

    delete_services.sh using nmap to identify node function

--- utils/delete_services.sh
+++ utils/delete_services.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ip_address=$(ip a show col0 | grep -Po "\b(?:\d{1,3}\.){3}\d{1,3}\b")
-third_octet=$(echo "$ip_address" | cut -d '.' -f 3)
-fourth_octet=$(echo "$ip_address" | cut -d '.' -f 4)
+source /root/OAI-Colosseum/utils/discover_function.sh
+interface_name="col0"
+result=$(get_host_index "$interface_name")
 
 systemctl disable --now start_network.service
-if [ "$third_octet" = "$fourth_octet" ]; then
+if [ "$result" = "1" ]; then
 	echo "Disabling BS+Core services"
 	systemctl disable --now core_network.service
 	docker rm -f $(docker ps -qa)
