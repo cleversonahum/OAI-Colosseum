edc4873468f104aaed2b96cf59fce2b8eab3b650
Cleverson Nahum <cleversonahum@gmail.com>
2024-02-28 12:11:27 -0500

    Reading scenario configuration from external file

---
+++ scenario_env.sh
@@ -0,0 +1,5 @@
+total_nodes=3
+ues_slice_1=1
+thr_slice_1=10
+thr_slice_2=20
+interface_name="col0"
--- utils/start_services.sh
+++ utils/start_services.sh
@@ -4,8 +4,7 @@ ip_address=$(ip a show col0 | grep -Po "\b(?:\d{1,3}\.){3}\d{1,3}\b")
 third_octet=$(echo "$ip_address" | cut -d '.' -f 3)
 
 source /root/OAI-Colosseum/utils/discover_function.sh
-interface_name="col0"
-total_nodes=3
+source /root/OAI-Colosseum/scenario_env.sh
 
 result=$(get_host_index "$interface_name" "$total_nodes")
 echo "Node idx $result"
@@ -23,8 +22,14 @@ else
 	echo "Enabling UE services"
 	first_three_octets=$(echo $ip_address | cut -d '.' -f 1-3)
 	gnb_ip="$first_three_octets.$third_octet"
-	sd=$(($result%2 + 1))
-	echo -e "GNB_IP=$gnb_ip\nSD=$sd\nNODE_IDX=$result" > /root/OAI-Colosseum/services/env.conf
+	if [ "$(($result - 1))" -le "$ues_slice_1" ]; then
+		sd=1
+		thr_ue=$thr_slice_1
+	else
+		sd=2
+		thr_ue=$thr_slice_2
+	fi
+	echo -e "GNB_IP=$gnb_ip\nSD=$sd\nNODE_IDX=$result\nTHR_UE=$thr_ue" > /root/OAI-Colosseum/services/env.conf
 	systemctl enable --now /root/OAI-Colosseum/services/ue.service
 	systemctl enable --now /root/OAI-Colosseum/services/iperf_client.service
 fi
