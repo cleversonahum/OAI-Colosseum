84ee63a9e53d1fa8aa142439def7094a37b29f24
Cleverson Nahum <cleversonahum@gmail.com>
2024-03-11 18:03:32 +0000

    Updating delete services to exclude docker-compose containers instead of all containers

--- utils/delete_services.sh
+++ utils/delete_services.sh
@@ -5,14 +5,8 @@ source /share/config/rl_ran_slicing/scenario.env
 systemctl disable --now start_network.service
 echo "Disabling BS+Core services"
 systemctl disable --now core_network.service
-docker rm -f $(docker ps -qa)
 systemctl disable --now gnb.service iperf_server@ xapp_server xapp_client
-
-#iperf_servers=$(($total_nodes-1))
-#while [ $iperf_servers -ne 0 ]; do
-#	systemctl disable --now iperf_server@$iperf_servers
-#	((iperf_servers--))  # Reduce the value of iperf_servers by one
-#done
+docker compose -f /root/OAI-Colosseum/core/docker-compose-mini-nrf.yaml down
 
 echo "Disabling UE services"
 systemctl disable --now ue iperf_client
