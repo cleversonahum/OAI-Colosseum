850547601790cd290f7d98e2f2bb1e667ad6f48a
Cleverson Nahum <cleversonahum@gmail.com>
2024-03-14 20:58:25 +0000

    Implementing a for to delete iperf_server services in case the systemd version does not support deleting with iperf_server@

--- utils/delete_services.sh
+++ utils/delete_services.sh
@@ -8,5 +8,11 @@ systemctl disable --now core_network.service
 systemctl disable --now gnb.service iperf_server@ xapp_server xapp_client
 docker compose -f /root/OAI-Colosseum/core/docker-compose-mini-nrf.yaml down
 
+iperf_servers=$(($total_nodes-1))
+while [ $iperf_servers -ne 0 ]; do
+	systemctl disable --now iperf_server@$iperf_servers
+	((iperf_servers--))  # Reduce the value of iperf_servers by one
+done
+
 echo "Disabling UE services"
 systemctl disable --now ue iperf_client
